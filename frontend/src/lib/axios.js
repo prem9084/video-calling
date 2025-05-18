0d68f43f2ea9ee0b55436955d1e7be2452b9a668
prem9084 <146803260+prem9084@users.noreply.github.com>
2025-05-18 18:02:04 +0530

    add

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,4 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://stream-phsz.onrender.com/api",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
