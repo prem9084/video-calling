afe370b9e11dabd241c596c10c51dfeb97e14a27
prem9084 <146803260+prem9084@users.noreply.github.com>
2025-05-19 13:54:11 +0530

    ad

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+
 export const axiosInstance = axios.create({
-  baseURL: "https://stream-phsz.onrender.com/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
