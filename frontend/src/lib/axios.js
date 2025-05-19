f2d0f2eafce8ee8bad462a7030535b4a1e63c322
prem9084 <146803260+prem9084@users.noreply.github.com>
2025-05-19 16:59:37 +0530

    add

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "https://video-calling-6grd.onrender.com/api"
+    : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
