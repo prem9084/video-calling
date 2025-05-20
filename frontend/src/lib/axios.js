6197624db337a33bc07c260eb0a77b6e1cdedfb1
prem9084 <146803260+prem9084@users.noreply.github.com>
2025-05-20 09:26:42 +0530

    add

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -6,7 +6,7 @@ import axios from "axios";
 //     : "/api";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://video-calling-6grd.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
